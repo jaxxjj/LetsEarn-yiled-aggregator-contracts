beb1a6b75d56822e1d6f66944989324ca669e027
Jackson Chen <yc5082@nyu.edu>
2024-11-25 08:12:20 -0500

    add more tests

--- test/LetsVault.t.sol
+++ test/LetsVault.t.sol
@@ -239,4 +239,127 @@ contract LetsVaultTest is Test {
         
         vm.stopPrank();
     }
+    
+    // Redeem Tests
+    function test_Redeem() public {
+        uint256 depositAmount = 100e18;
+        
+        // Setup - deposit first
+        vm.prank(user);
+        vault.deposit(depositAmount, user);
+        
+        vm.startPrank(user);
+        
+        vm.expectEmit(true, true, true, true);
+        emit Withdraw(user, user, user, depositAmount, depositAmount);
+        
+        uint256 assets = vault.redeem(depositAmount, user, user);
+        
+        assertEq(assets, depositAmount);
+        assertEq(vault.totalAssets(), 0);
+        assertEq(vault.balanceOf(user), 0);
+        assertEq(underlying.balanceOf(user), INITIAL_BALANCE);
+        
+        vm.stopPrank();
+    }
+
+    
+    // Strategy Management Tests
+    function test_MultipleStrategies() public {
+        MockStrategy strategy2 = new MockStrategy(address(underlying));
+        
+        // Manager actions - add strategies
+        vm.startPrank(manager);
+        vault.addStrategy(address(strategy), 500e18);
+        vault.addStrategy(address(strategy2), 500e18);
+        vm.stopPrank();
+        
+        // User action - deposit
+        vm.startPrank(user);
+        vault.deposit(1000e18, user);
+        vm.stopPrank();
+        
+        // Manager actions - update debts
+        vm.startPrank(manager);
+        vault.updateDebt(address(strategy), 300e18);
+        vault.updateDebt(address(strategy2), 400e18);
+        
+        assertEq(vault.totalDebt(), 700e18);
+        assertEq(vault.totalIdle(), 300e18);
+        
+        // Verify strategy balances
+        (,,uint256 debt1,) = vault.strategies(address(strategy));
+        (,,uint256 debt2,) = vault.strategies(address(strategy2));
+        assertEq(debt1, 300e18);
+        assertEq(debt2, 400e18);
+        vm.stopPrank();
+    }
+    
+    function test_MaxDebtLimit() public {
+        uint256 maxDebt = 500e18;
+        
+        // Manager adds strategy
+        vm.startPrank(manager);
+        vault.addStrategy(address(strategy), maxDebt);
+        vm.stopPrank();
+        
+        // User deposits
+        vm.startPrank(user);
+        vault.deposit(1000e18, user);
+        vm.stopPrank();
+        
+        // Manager updates debt
+        vm.startPrank(manager);
+        // Try to exceed max debt
+        vm.expectRevert("Exceeds max debt");
+        vault.updateDebt(address(strategy), maxDebt + 1);
+        
+        // Should work within limit
+        vault.updateDebt(address(strategy), maxDebt);
+        (,,uint256 currentDebt,) = vault.strategies(address(strategy));
+        assertEq(currentDebt, maxDebt);
+        vm.stopPrank();
+    }
+    
+    
+    function test_ManagerOnlyFunctions() public {
+        vm.prank(user);
+        vm.expectRevert("Not manager");
+        vault.addStrategy(address(strategy), 1000e18);
+        
+        vm.prank(user);
+        vm.expectRevert("Not manager");
+        vault.pause();
+    }
+    
+    // View Function Tests
+    function test_TotalAssets() public {
+        vm.prank(user);
+        vault.deposit(1000e18, user);
+        
+        vm.startPrank(manager);
+        vault.addStrategy(address(strategy), 1000e18);
+        vault.updateDebt(address(strategy), 600e18);
+        
+        assertEq(vault.totalAssets(), 1000e18);
+        assertEq(vault.totalDebt(), 600e18);
+        assertEq(vault.totalIdle(), 400e18);
+    }
+    
+    function test_MaxDepositPaused() public {
+        vm.prank(manager);
+        vault.pause();
+        assertEq(vault.maxDeposit(user), 0);
+    }
+    
+    function test_MaxWithdraw() public {
+        uint256 depositAmount = 100e18;
+        
+        vm.prank(user);
+        vault.deposit(depositAmount, user);
+        
+        assertEq(vault.maxWithdraw(user), depositAmount);
+    }
+    
+
 } 
